842afcc7587dcaa8b8fc288eb6910fdeff61362a
Wagner Silveira Souto <wagner.souto@ifood.com.br>
2024-02-06 08:41:34 -0300

    feature/Upload project made with TS

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import './global.css';
+
+import styles from './App.module.css';
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { PostType } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego 3g',
+      role: 'noooob @ Github',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala aeh' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no portfolio. Passem lá pra dar uma moral',
+      },
+      { type: 'link', content: 'chuchu.beleza/nonsense_projects' },
+    ],
+    publishedAt: new Date('2023-12-26 20:40:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @ RocketSeat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala, galera' },
+      {
+        type: 'paragraph',
+        content:
+          'Ansioso para ver os projetos do curso saindo. Comentem seus links aí'
+      },
+      { type: 'link', content: 'repo.curso/nonsense_projects' },
+    ],
+    publishedAt: new Date('2023-12-26 20:00:00'),
+  },
+];
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                post={post}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </>
+  );
+}
